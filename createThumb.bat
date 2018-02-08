60f42d1bb07f90a13507780d52d922b7cfe9961a
samreay <samuelreay@gmail.com>
2018-02-08 18:27:24 +1000

    Adding progressive

--- createThumb.bat
+++ createThumb.bat
@@ -6,8 +6,8 @@ cd static\img\%1
 set img=%2
 if not defined img set img="main.jpg"
 echo %img%
-for /R %%U in (*.png) do echo %%U && magick mogrify -resize "1000>" -sampling-factor 4:2:0 -strip -quality 80 -format jpg %%U
-magick %img% -sampling-factor 4:2:0 -strip -quality 60 -resize 120x120^^ -gravity center -crop 120x120+0+0 +repage thumb_blog.jpg
-magick %img% -sampling-factor 4:2:0 -strip -quality 60 -resize 180x120^^ -gravity center -crop 180x120+0+0 +repage thumb_outreach.jpg
-magick %img% -sampling-factor 4:2:0 -strip -quality 70 -resize 350x200^^ -gravity center -crop 350x200+0+0 +repage thumb_project.jpg
-magick %img% -sampling-factor 4:2:0 -strip -quality 70 -resize 500x250^^ -gravity center -crop 500x250+0+0 +repage thumb_fb.jpg
+for /R %%U in (*.png) do echo %%U && magick mogrify -resize "1000>" -sampling-factor 4:2:0 -interlace Plane -strip -quality 80 -format jpg %%U
+magick %img% -sampling-factor 4:2:0 -strip -quality 60 -resize 120x120^^ -gravity center -interlace Plane -crop 120x120+0+0 +repage thumb_blog.jpg
+magick %img% -sampling-factor 4:2:0 -strip -quality 60 -resize 180x120^^ -gravity center -interlace Plane -crop 180x120+0+0 +repage thumb_outreach.jpg
+magick %img% -sampling-factor 4:2:0 -strip -quality 70 -resize 350x200^^ -gravity center -interlace Plane -crop 350x200+0+0 +repage thumb_project.jpg
+magick %img% -sampling-factor 4:2:0 -strip -quality 70 -resize 500x250^^ -gravity center -interlace Plane -crop 500x250+0+0 +repage thumb_fb.jpg
