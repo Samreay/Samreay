c7f0f11fcd8f3df836260e9afbfae66626bff479
samreay <samuelreay@gmail.com>
2018-02-09 14:54:33 +1000

    Adding slow motion sim

--- createThumb.bat
+++ createThumb.bat
@@ -12,9 +12,9 @@ for /R %%U in (*.jpg) do echo %%U && magick mogrify -resize "1000>" -sampling-fa
 cd ..
 rmdir thumbs
 mkdir thumbs
-for /R %%U in (*.jpg) do echo %%U && magick mogrify -resize "16x16^" -sampling-factor 4:2:0 -format jpg -path thumbs -strip -quality 60 %%U
-magick %img% -sampling-factor 4:2:0 -strip -quality 60 -resize 120x120^^ -gravity center -interlace Plane -crop 120x120+0+0 +repage thumb_blog.jpg
-magick %img% -sampling-factor 4:2:0 -strip -quality 60 -resize 180x120^^ -gravity center -interlace Plane -crop 180x120+0+0 +repage thumb_outreach.jpg
+magick %img% -sampling-factor 4:2:0 -strip -quality 70 -resize 120x120^^ -gravity center -interlace Plane -crop 120x120+0+0 +repage thumb_blog.jpg
+magick %img% -sampling-factor 4:2:0 -strip -quality 70 -resize 180x120^^ -gravity center -interlace Plane -crop 180x120+0+0 +repage thumb_outreach.jpg
 magick %img% -sampling-factor 4:2:0 -strip -quality 70 -resize 350x200^^ -gravity center -interlace Plane -crop 350x200+0+0 +repage thumb_project.jpg
 magick %img% -sampling-factor 4:2:0 -strip -quality 70 -resize 500x250^^ -gravity center -interlace Plane -crop 500x250+0+0 +repage thumb_fb.jpg
+for /R %%U in (*.jpg) do echo %%U && magick mogrify -resize "16x16^" -sampling-factor 4:2:0 -format jpg -path thumbs -strip -quality 60 %%U
 
