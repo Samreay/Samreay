245d6ce25e79418b9d04b01019b3df24ae1bac1c
Samuel Hinton <samuelreay@gmail.com>
2018-06-27 10:30:49 +1000

    Updating bio

--- runLocal.bat
+++ runLocal.bat
@@ -1,2 +1,2 @@
 rmdir _site /s /q
-bundle exec jekyll serve --watch --incremental --limit-posts 3 --config _config.yml,_config-dev.yml
+bundle exec jekyll serve --watch --incremental --config _config.yml,_config-dev.yml
