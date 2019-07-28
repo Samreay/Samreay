e7a03b2b7f098c5483b6214fceee3193a09dc831
Samuel Hinton <samuelreay@gmail.com>
2019-07-28 18:25:45 -0400

    Adding convert_notebook

---
+++ convert_notebook.py
@@ -0,0 +1,71 @@
+import os
+import subprocess
+import shutil
+import sys
+
+name = sys.argv[1]
+print(f"Converting {name}")
+
+assert os.path.exists(name)
+basename = os.path.basename(name).lower().split(".")[0]
+short_name = basename.split("-")[-1]
+print(f"Short name is {short_name}")
+
+basedir = f"_posts/blog/"
+print("Calling convert")
+subprocess.run(f"jupyter nbconvert {name} --to markdown --output-dir {basedir}", check=True)
+
+
+print("Moving images around")
+img_dir = f"static/img/blog/{short_name}"
+if os.path.exists(img_dir):
+    print("Removing images")
+    shutil.rmtree(img_dir)
+os.makedirs(img_dir, exist_ok=True)
+output_img_dir = os.path.join(basedir, f"{basename}_files")
+desired_img_dir = os.path.join(img_dir, "original")
+shutil.move(output_img_dir, desired_img_dir)
+
+# Process MD file
+print("Processing file")
+
+md_file = f"{basedir}/{basename}.md"
+with open(md_file, 'r') as fin:
+    data = fin.read().splitlines(True)[1:]
+
+main_img = None
+img_index = None
+for i, l in enumerate(data):
+    if l.startswith("!!!replace"):
+        name2 = name.replace('\\', '/')
+        data[i] = ""
+        # data[i] = f"[Download the notebook here](https://cosmiccoding.com.au/{name2})"
+    if l.startswith("![png]"):
+        loc = l.split("[png]")[1][1:-2].split("/")[1]
+        replacement = f'{{% include image.html url="{loc}"  %}}'
+        print(f"Replacing image insert {loc}")
+        data[i] = replacement
+        img_index = i
+    if l.startswith("!!!main"):
+        main_img = loc
+        data[img_index] = data[img_index].replace(loc, "main.png")
+        print(f"Found main image {loc}")
+        data[i] = ""
+
+with open(md_file, 'w') as fout:
+    fout.writelines(data)
+
+# Rename the right image to main
+if main_img is None:
+    print("WAT NO MAIN IMAGE FOUND, ADD A MARKDOWN '!!!main' after the image you want as the thumbnail")
+else:
+    print(f"Main image is found as {main_img}")
+    shutil.move(os.path.join(desired_img_dir, loc), os.path.join(desired_img_dir, "main.png"))
+
+# Process images
+print("Processing images")
+subprocess.run(["createThumb.bat", f"blog/{short_name}"], check=True)
+
+print("Updating thumbs")
+subprocess.run("python crunch.py", check=True)
+
