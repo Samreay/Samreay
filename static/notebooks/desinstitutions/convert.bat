b865bce2d4e786a144a0b567472b8b3c6fb41aab
Samuel Hinton <samuelreay@gmail.com>
2019-08-11 11:12:29 -0400

    Updating video to be a bit slower

--- static/notebooks/desinstitutions/convert.bat
+++ static/notebooks/desinstitutions/convert.bat
@@ -1,1 +1,1 @@
-ffmpeg -r 30 -f image2 -i output/out_%%04d.png -s 1920x980 -vcodec libx264 -crf 25  -pix_fmt yuv420p test.mp4
+ffmpeg -r 24 -f image2 -i output/out_%%04d.png -s 1920x980 -vcodec libx264 -crf 20  -pix_fmt yuv420p des.mp4
