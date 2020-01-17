adeae09cade6c1907dc9e1e6816e1f281673fc8d
Samuel Hinton <samuelreay@gmail.com>
2020-01-17 13:52:33 +1000

    Adding one page resume

--- static/resume/make.bat
+++ static/resume/make.bat
@@ -1,3 +1,4 @@
 xelatex HintonCV && biber HintonCV && xelatex HintonCV
 xelatex HintonResume && biber HintonResume && xelatex HintonResume
 xelatex HintonPublications && biber HintonPublications && xelatex HintonPublications
+xelatex -interaction nonstopmode -halt-on-error HintonOnePage || xelatex -interaction nonstopmode -halt-on-error HintonOnePage
