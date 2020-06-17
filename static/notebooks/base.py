e5b588cb0845a0144ab28d603f66fdb803b63ac6
Samuel Hinton <samuelreay@gmail.com>
2020-06-18 00:00:48 +1000

    Adding snippet to remove empty columns

---
+++ static/notebooks/base.py
@@ -0,0 +1,25 @@
+
+# manim imports
+import jupyter_manim
+from manimlib.imports import *
+
+# Matplotlib imports
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (8, 4)
+plt.rcParams['figure.dpi'] = 200
+plt.rcParams['hist.bins'] = 50
+plt.rcParams['lines.markersize'] = 2
+plt.rcParams["axes.labelsize"] = 14
+plt.rcParams["ytick.labelsize"] = 12
+plt.rcParams["xtick.labelsize"] = 12
+plt.rc('text', usetex=False)
+plt.rc('font', family='sans-serif')
+plt.rcParams['legend.frameon'] = False
+
+from cycler import cycler	  
+plt.rcParams['axes.prop_cycle'] = (cycler(color=['#003049', '#D62828', '#F77F00', '#FCBF49', '#EAE2B7']) + cycler(linestyle=['-', '--', ':', '-.', '-']))
+
+class DefaultScene(Scene):
+    CONFIG={
+        "camera_config":{"background_color":"#FFFFFF"}
+    }
