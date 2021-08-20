9b74673088fb7f54eeae5ad7bd19c4db371c1ccc
Samuel Hinton <samuelreay@gmail.com>
2021-08-20 22:37:45 +0100

    Adding share link back

--- static/styles/index.js
+++ static/styles/index.js
@@ -223,7 +223,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58611" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55321" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
