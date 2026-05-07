1e18922369b28917523bcd7cc838a3033a2eaa86
Samuel Hinton <samreay@canva.com>
2026-05-07 16:08:31 +1000

    Adding flowchart

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,6 +1,7 @@
 export const nav = [
   { label: 'Books', link: '/#books' },
   { label: 'Reviews', link: '/reviews' },
+  { label: 'Flowchart', link: '/reviews/flowchart' },
   { label: 'Tutorials', link: '/tutorials' },
   { label: 'Blog', link: '/blogs' },
   { label: 'Artists', link: '/artists' },
